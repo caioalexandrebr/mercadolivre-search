6aa56be5fa47b4b6dcde1cb474483db96d8058de
Caio Alexandre <contato@caioalexandre.com>
2020-10-07 17:17:26 -0300

    feat(List): add list component

---
+++ src/components/List/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function List() {
+  return (
+    <section className='list'>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-12'>
+            <ul className='list__content'>
+              <li className='list__card'></li>
+              <li className='list__card'></li>
+              <li className='list__card'></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default List;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './sass/style.scss';
 
 import Header from './components/Header';
+import List from './components/List';
 
 ReactDOM.render(
   <React.StrictMode>
     <Header />
+    <List />
   </React.StrictMode>,
   document.getElementById('root')
 );

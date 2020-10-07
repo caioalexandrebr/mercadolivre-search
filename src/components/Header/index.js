f80ecd90ca5c95dad809322a07275a115aeafbda
Caio Alexandre <contato@caioalexandre.com>
2020-10-07 13:27:38 -0300

    first commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './style.scss'
+
+function Header() {
+  return (
+    <section className='header'>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-12'>
+            Olá mundo!
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Header;

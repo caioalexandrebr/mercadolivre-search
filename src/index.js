5087067cb173ffd8577a60a6464783e943b4bc80
Caio Alexandre <contato@caioalexandre.com>
2020-10-07 18:02:10 -0300

    feat(Package): add react-router as dependency

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './sass/style.scss';
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+
 import Header from './components/Header';
 import Breadcrumb from './components/Breadcrumb';
 import List from './components/List';
@@ -9,8 +15,21 @@ import List from './components/List';
 ReactDOM.render(
   <React.StrictMode>
     <Header />
-    <Breadcrumb />
-    <List />
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <h1>Home</h1>
+        </Route>
+        <Route path="/search">
+          <Breadcrumb />
+          <List />
+        </Route>
+        <Route path="/product">
+          <Breadcrumb />
+          <h1>Product</h1>
+        </Route>
+      </Switch>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );

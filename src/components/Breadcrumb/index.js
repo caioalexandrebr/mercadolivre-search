63732e39c3ce976766699a6ad3b7ae4ed7a20199
Caio Alexandre <contato@caioalexandre.com>
2020-10-07 17:36:41 -0300

    feat(Breadcrumb): add breadcrumb component

---
+++ src/components/Breadcrumb/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Breadcrumb() {
+  return (
+    <section className='breadcrumb'>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-12'>
+            <span><a href='/'>Informática</a></span>
+            <span><a href='/'>Acessórios para PC Gaming</a></span>
+            <span><a href='/'>Fones</a></span>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Breadcrumb;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import './sass/style.scss';
 
 import Header from './components/Header';
+import Breadcrumb from './components/Breadcrumb';
 import List from './components/List';
 
 ReactDOM.render(
   <React.StrictMode>
     <Header />
+    <Breadcrumb />
     <List />
   </React.StrictMode>,
   document.getElementById('root')

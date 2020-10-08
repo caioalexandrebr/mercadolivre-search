fa072fb54b4600019e53843cbb92fdf8802fcde6
Caio Alexandre <contato@caioalexandre.com>
2020-10-08 12:19:43 -0300

    feat(Package/List): add axios library for api request

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,21 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function List() {
-  return (
-    <section className='list'>
-      <div className='container'>
-        <div className='row'>
-          <div className='col-12'>
-            <ul className='list__content'>
-              <li className='list__card'></li>
-              <li className='list__card'></li>
-              <li className='list__card'></li>
-            </ul>
+const axios = require('axios');
+
+class List extends Component {
+  state = {
+    products: [],
+  }
+
+  async componentDidMount() {
+    const response = await axios.get('https://api.mercadolibre.com/sites/MLA/search?q=laptop&limit=4');
+    console.log(response.data.results);
+    this.setState({ products: response.data.results });
+  }
+
+  render() {
+    const { products } = this.state;
+
+    return (
+      <section className='list'>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-12'>
+              <ul className='list__content'>
+                {products.map(product => (
+                  <li key={product.id} className='list__card'>
+                    <div className='list__image'>
+                      <img src={product.thumbnail} alt={product.title} />
+                    </div>
+                    <div className='list__description'>
+                      <p className='list__price'>$ {product.price}</p>
+                      <p className='list__title'>{product.title}</p>
+                      <p className='list__address'>{product.address.state_name}</p>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
-  );
-}
+      </section>
+    );
+  };
+};
 
 export default List;
